c6f2cc0155f297b6fe488f0022bd3b924b2901b3
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-07-16 23:29:40 +0600

    feat: add sign in layout

--- client/src/page/sign-in/index.tsx
+++ client/src/page/sign-in/index.tsx
@@ -1,5 +1,32 @@
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <section className="flex flex-col gap-10">
+      <h1>Welcome to Audio Chat</h1>
+      <form action="" className="flex flex-col justify-center m-auto gap-4">
+        <div className="">
+          <label htmlFor="">UserName: </label>
+          <input
+            type="text"
+            placeholder="Type here UserName"
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+        </div>
+
+        <div className="">
+          <label htmlFor="">Name: </label>
+          <input
+            type="text"
+            placeholder="Type here Name"
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-outline btn-primary">
+          Sign in
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SignIn;
