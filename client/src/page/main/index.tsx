0a227a536dba86e6bc05e47a8cb20000056a53e5
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-07-22 19:05:57 +0600

    feat: create the main page

--- client/src/page/main/index.tsx
+++ client/src/page/main/index.tsx
@@ -1,15 +1,65 @@
 import { StreamVideo } from "@stream-io/video-react-sdk";
 import { useUser } from "../../shared/UserContext/ui/user-context";
 import { Navigate } from "react-router-dom";
+import { useState } from "react";
+
+interface NewRoom {
+  name: string;
+  description: string;
+}
 
 const MainPage = () => {
-  const { client } = useUser();
+  const { client, user } = useUser();
+  const [newRoom, setNewRoom] = useState<NewRoom>({ name: "", description: "" });
 
   if (!client) return <Navigate to="/sign-in" />;
 
+  const createRoom = async () => {
+    const { name, description } = newRoom;
+
+    if (!client || !name || !description || !user) return;
+
+    const call = client.call("audio_room", name);
+    await call.join({
+      create: true,
+      data: {
+        members: [{ user_id: user.username }],
+        custom: {
+          title: name,
+          description,
+        },
+      },
+    });
+  };
+
   return (
     <StreamVideo client={client}>
-      <div className="">MainPage</div>
+      <section className="text-xl">
+        <h1>Welcome, {user?.name}</h1>
+        <form onSubmit={createRoom} className="flex flex-col gap-3 text-center items-center pt-6">
+          <h2>Create Your Own Room</h2>
+          <input
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setNewRoom(prev => ({ ...prev, name: e.target.value }))
+            }
+            type="text"
+            placeholder="Room Name..."
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+          <input
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setNewRoom(prev => ({ ...prev, description: e.target.value }))
+            }
+            type="text"
+            placeholder="Room Description..."
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+
+          <button type="submit" className="btn btn-outline btn-primary">
+            Sign in
+          </button>
+        </form>
+      </section>
     </StreamVideo>
   );
 };
