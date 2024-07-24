7a6c7fb3ece37f288a80d20f69a71b8734a8470d
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-07-24 20:03:10 +0600

    feat: output of available rooms

--- client/src/page/main/index.tsx
+++ client/src/page/main/index.tsx
@@ -1,6 +1,6 @@
-import { StreamVideo } from "@stream-io/video-react-sdk";
+import { Call, StreamVideo } from "@stream-io/video-react-sdk";
 import { Navigate, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CryptoJS from "crypto-js";
 
 import { useUser } from "../../shared/UserContext/ui/user-context";
@@ -10,12 +10,30 @@ interface NewRoom {
   description: string;
 }
 
+interface Room {
+  id: string;
+  title: string;
+  description: string;
+  participantsLength: number;
+  createBy: string;
+}
+
+type CustomCallData = {
+  title?: string;
+  description?: string;
+};
+
 const MainPage = () => {
   const { client, user, setCall, isLoadingClient } = useUser();
   const [newRoom, setNewRoom] = useState<NewRoom>({ name: "", description: "" });
+  const [availableRooms, setavailableRooms] = useState<Room[]>([]);
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (client) fetchListOfCalls();
+  }, [client]);
 
   const hashRoomName = (roomName: string): string => {
     const hash = CryptoJS.SHA256(roomName).toString(CryptoJS.enc.Base64);
@@ -42,8 +60,41 @@ const MainPage = () => {
     navigate("/room");
   };
 
-  if (isLoadingClient) return <h1>Loading...</h1>;
+  const fetchListOfCalls = async () => {
+    const callsQueryResponse = await client?.queryCalls({
+      filter_conditions: { ongoing: true },
+      limit: 4,
+      watch: true,
+    });
 
+    if (!callsQueryResponse) {
+      alert("No calls");
+    } else {
+      const getCallInfo = async (call: Call): Promise<Room> => {
+        const callInfo = await call.get();
+        const customData = callInfo.call.custom;
+        const { title, description } = (customData || {}) as CustomCallData;
+        const participantsLength = callInfo.members.length ?? 0;
+        const createBy = callInfo.call.created_by.name ?? "";
+        const id = callInfo.call.id ?? "";
+
+        return {
+          id,
+          title: title ?? "",
+          description: description ?? "",
+          participantsLength,
+          createBy,
+        };
+      };
+
+      const roomPromises = await callsQueryResponse.calls.map(call => getCallInfo(call));
+
+      const rooms = await Promise.all(roomPromises);
+      setavailableRooms(rooms);
+    }
+  };
+
+  if (isLoadingClient) return <h1>Loading...</h1>;
   if ((!isLoadingClient && !user) || (!isLoadingClient && !client)) return <Navigate to="/sign-in" />;
 
   return (
@@ -72,6 +123,26 @@ const MainPage = () => {
           <button onClick={createRoom} className="btn btn-outline btn-primary">
             Create room
           </button>
+
+          {availableRooms.length !== 0 ? (
+            <>
+              <h2>Available Rooms</h2>
+              <div className="">
+                {availableRooms.map(room => (
+                  <div className="card bg-primary text-primary-content w-72 text-left" key={room.id}>
+                    <div className="card-body">
+                      <h1 className="card-title">{room.title}</h1>
+                      <h2>{room.description}</h2>
+                      <p>Participants: {room.participantsLength}</p>
+                      <p>Created By {room.createBy}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          ) : (
+            <h2>No available rooms at the moment</h2>
+          )}
         </div>
       </section>
     </StreamVideo>
